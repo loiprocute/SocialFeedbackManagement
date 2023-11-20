d1dcbec011020f9ec7f5e784d5ead806d2139fc6
loiprocute <huuloi312001@gmail.com>
2023-11-21 00:11:34 +0700

    final

--- Kafka/customer.py
+++ Kafka/customer.py
@@ -4,28 +4,20 @@ import pyodbc
 
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=DESKTOP-O7T6R5H;'
-                      'Database=feedback;'
+                      'Database=feedback_store;'
                       'Trusted_Connection=yes;')
 
 cursor = conn.cursor()
 
 consumer = KafkaConsumer('topic-example', bootstrap_servers='localhost:9092', group_id='my-group')
 for message in consumer:
-    #print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
-    #print(str(message.value))
     value = message.value.decode('utf-8')
     value =  tuple(value.split('==='))
-    # user,text_review,rate,time_visit,language,platform = value
-    #print(user,rate,time_visit,language,platform)
-    query =  'INSERT INTO [testdbo] ([user],[text_review],[rate],[time_visit],[language],[platform]) VALUES (?, ?, ?, ?, ?, ?)'
-    #print(query)
-    print("Insert data into table : ",value[0],value[-1])
-    cursor.execute(query,value)
+    name_producer =  value[0]
+    print("receive data from {}".format(name_producer))
+    query =  'INSERT INTO [feedback] ([user],[text_review],[sentiment_review],[sentiment_score],[rate],[time_visit],[language],[platform]) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+    print("Insert data into table SQL : ",value[1],value[-1])
+    cursor.execute(query,value[1:])
     conn.commit()
-    print("Successfully !!!")
-    # try:
-    #     json_data = json.loads(message.value().decode('utf-8'))
-    #     # Xử lý dữ liệu JSON ở đây
-    #     print('Received JSON data:', json_data)
-    # except json.JSONDecodeError as e:
-    #     print('Error decoding JSON:', e)
+    print("sent to SQL successfully !")
+   
